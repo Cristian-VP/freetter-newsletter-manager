e6567768e9f5abfb78d6dee5d6a4288ddbd007c4
Cristian-VP <cvacacelaprocel@cifpfbmoll.eu>
2026-02-02 14:19:39 +0000

    Added: Activity domain

---
+++ app-modules/activity/src/Providers/ActivityServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Domains\Activity\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ActivityServiceProvider extends ServiceProvider
+{
+	public function register(): void
+	{
+	}
+	
+	public function boot(): void
+	{
+	}
+}
---
+++ app-modules/activity/tests/Feature/Providers/ActivityServiceProviderTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Domains\Activity\Tests;
+
+class ActivityServiceProviderTest extends TestCase
+{
+	// TODO
+}
