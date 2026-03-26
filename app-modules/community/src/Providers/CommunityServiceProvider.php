9eb49d55d08ce666500c0e1ea06d8bfe1ee19d32
Cristian-VP <cvacacelaprocel@cifpfbmoll.eu>
2026-03-26 15:34:43 +0000

    Fixed: Comunity  listeners, providers and test

--- app-modules/community/src/Providers/CommunityServiceProvider.php
+++ app-modules/community/src/Providers/CommunityServiceProvider.php
@@ -6,11 +6,10 @@ use Illuminate\Support\ServiceProvider;
 
 class CommunityServiceProvider extends ServiceProvider
 {
-	public function register(): void
-	{
-	}
-	
-	public function boot(): void
-	{
-	}
+    public function register(): void {}
+
+    public function boot(): void
+    {
+        $this->loadRoutesFrom(__DIR__.'/../../routes/community-routes.php');
+    }
 }
