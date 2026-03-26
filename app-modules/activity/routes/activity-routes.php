2bf55253ca472aba2bfda780d68d2451f77b0768
Cristian-VP <cvacacelaprocel@cifpfbmoll.eu>
2026-03-26 15:33:40 +0000

    Fixed: Activity listeners, providers and test

--- app-modules/activity/routes/activity-routes.php
+++ app-modules/activity/routes/activity-routes.php
@@ -1,11 +1,8 @@
 <?php
 
-// use Domains\Activity\Http\Controllers\ActivityController;
+use Domains\Activity\Http\Controllers\ActivityLogController;
+use Illuminate\Support\Facades\Route;
 
-// Route::get('/activities', [ActivityController::class, 'index'])->name('activities.index');
-// Route::get('/activities/create', [ActivityController::class, 'create'])->name('activities.create');
-// Route::post('/activities', [ActivityController::class, 'store'])->name('activities.store');
-// Route::get('/activities/{activity}', [ActivityController::class, 'show'])->name('activities.show');
-// Route::get('/activities/{activity}/edit', [ActivityController::class, 'edit'])->name('activities.edit');
-// Route::put('/activities/{activity}', [ActivityController::class, 'update'])->name('activities.update');
-// Route::delete('/activities/{activity}', [ActivityController::class, 'destroy'])->name('activities.destroy');
+Route::prefix('activity')->name('activity.')->group(function (): void {
+    Route::get('/logs', [ActivityLogController::class, 'index'])->name('logs.index');
+});
