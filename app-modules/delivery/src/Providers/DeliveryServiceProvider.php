b97876674f158ef99da5a648181774bf7785cee8
Cristian-VP <cvacacelaprocel@cifpfbmoll.eu>
2026-04-06 15:54:58 +0000

    Added: service provides

--- app-modules/delivery/src/Providers/DeliveryServiceProvider.php
+++ app-modules/delivery/src/Providers/DeliveryServiceProvider.php
@@ -10,6 +10,7 @@ class DeliveryServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
         $this->loadRoutesFrom(__DIR__.'/../../routes/delivery-routes.php');
     }
 }
