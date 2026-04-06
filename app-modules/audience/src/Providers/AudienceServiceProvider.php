1c80d13b3ba9aa144b625f60c461f19fc7ec102c
Cristian-VP <cvacacelaprocel@cifpfbmoll.eu>
2026-04-06 14:44:15 +0000

    Added: service provider

--- app-modules/audience/src/Providers/AudienceServiceProvider.php
+++ app-modules/audience/src/Providers/AudienceServiceProvider.php
@@ -2,14 +2,23 @@
 
 namespace Domains\Audience\Providers;
 
+use Domains\Audience\Console\Commands\CleanupExpiredImportJobsCommand;
 use Illuminate\Support\ServiceProvider;
 
 class AudienceServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                CleanupExpiredImportJobsCommand::class,
+            ]);
+        }
+    }
 
     public function boot(): void
     {
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
         $this->loadRoutesFrom(__DIR__.'/../../routes/audience-routes.php');
     }
 }
