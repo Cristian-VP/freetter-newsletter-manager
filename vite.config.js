7caaa4955b26d1e92e6d5bab9eb61293378bdee8
Cristian Vacacela <cvacacelaprocel@cifpfbmoll.eu>
2026-04-08 13:39:25 +0000

    Changed:  new npm packages, vite config and global config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import react from "@vitejs/plugin-react";
+import laravel from "laravel-vite-plugin";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
+  plugins: [
+    laravel({
+      input: ["resources/css/app.css", "resources/js/app.tsx"],
+      ssr: "resources/js/ssr.jsx",
+      refresh: true,
+    }),
+    react(),
+    tsconfigPaths(), // Resolve path aliases in tsconfig.json.
+  ],
+  esbuild: {
+    jsx: "automatic",
+  },
 });
